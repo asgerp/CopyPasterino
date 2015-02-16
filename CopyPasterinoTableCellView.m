cee32d4f03b101fe66cecd34d92ea4bbe90f46be
Asger Pedersen <asger.pedersen@gmail.com>
2015-02-16 10:46:42 +0100

    Cellview implementation

---
+++ CopyPasterinoTableCellView.m
@@ -0,0 +1,29 @@
+//
+//  CopyPasterinoTableCellView.m
+//  CopyPasterino
+//
+//  Created by Asger Pedersen on 11/08/14.
+//  Copyright (c) 2014 Asger Pedersen. All rights reserved.
+//
+
+#import "CopyPasterinoTableCellView.h"
+
+@implementation CopyPasterinoTableCellView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
