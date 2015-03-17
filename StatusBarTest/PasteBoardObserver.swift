aacae1930b303d2a4ace641a87b0db5b1353c20d
Asger Pedersen <asger.pedersen@gmail.com>
2015-03-17 11:59:02 +0100

    observer subscriber

---
+++ StatusBarTest/PasteBoardObserver.swift
@@ -0,0 +1,9 @@
+//
+//  PasteBoardObserver.swift
+//  CopyPasterino
+//
+//  Created by Asger Pedersen on 17/02/15.
+//  Copyright (c) 2015 Asger Pedersen. All rights reserved.
+//
+
+import Foundation
---
+++ StatusBarTest/PasteBoardSubscriber.swift
@@ -0,0 +1,9 @@
+//
+//  PasteBoardSubscriber.swift
+//  CopyPasterino
+//
+//  Created by Asger Pedersen on 16/03/15.
+//  Copyright (c) 2015 Asger Pedersen. All rights reserved.
+//
+
+import Foundation
