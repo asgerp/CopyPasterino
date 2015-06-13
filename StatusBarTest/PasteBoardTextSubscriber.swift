af16af8d3d78d02518d0b2f99c6a8597bcbd48ec
Asger Pedersen <asger.pedersen@gmail.com>
2015-06-13 11:59:18 +0200

    check if pasteboard item is string

--- StatusBarTest/PasteBoardTextSubscriber.swift
+++ StatusBarTest/PasteBoardTextSubscriber.swift
@@ -16,25 +16,24 @@ class PasteBoardTextSubscriber: NSObject, PasteBoardSubscriber {
         
         let appDelegate = NSApplication.sharedApplication().delegate as! AppDelegate
         
-        let myString = pasteboard.stringForType(NSPasteboardTypeString)!
-        var menu:NSMenu = appDelegate.statusItem!.menu!
-        var numberOfItems = menu.numberOfItems + 1
-        var menuItem:NSMenuItem = NSMenuItem(title: myString, action: Selector("setPasteBoardString:"),
-            keyEquivalent: String(numberOfItems))
-        menuItem.representedObject = menuItem
-        menuItem.target = self
-        menuItem.enabled = true
-        menu.addItem(menuItem)
+        if let myString = pasteboard.stringForType(NSPasteboardTypeString) {
+            var menu:NSMenu = appDelegate.statusItem!.menu!
+            var numberOfItems = menu.numberOfItems + 1
+            var menuItem:NSMenuItem = NSMenuItem(title: myString, action: Selector("setPasteBoardString:"),
+                keyEquivalent: String(numberOfItems))
+            menuItem.representedObject = menuItem
+            menuItem.target = self
+            menuItem.enabled = true
+            menu.addItem(menuItem)
         
-        let activeApp = NSWorkspace.sharedWorkspace().frontmostApplication
+            let activeApp = NSWorkspace.sharedWorkspace().frontmostApplication
+            let activeAppName = activeApp?.localizedName
+            let image = activeApp?.icon?.copy() as! NSImage
         
-        let activeAppName = activeApp?.localizedName
-        
-        let image = activeApp?.icon?.copy() as! NSImage
-        
-        var paste:Paste = Paste(paste: myString, name: activeAppName!, image: image)
-        appDelegate._tableContents.insert(paste, atIndex: 0)
-        appDelegate.tableView.reloadData()
+            var paste:Paste = Paste(paste: myString, name: activeAppName!, image: image)
+            appDelegate._tableContents.insert(paste, atIndex: 0)
+            appDelegate.tableView.reloadData()
+        }
         
     }
 }
